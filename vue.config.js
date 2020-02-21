fc0025f936e6464525b910979f99405f405128e9
ldemon1005 <ldemon1005@gmail.com>
2020-02-21 16:41:18 +0700

    master

--- vue.config.js
+++ vue.config.js
@@ -29,7 +29,6 @@ module.exports = {
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
-    port: port,
     open: true,
     overlay: {
       warnings: false,
@@ -39,7 +38,6 @@ module.exports = {
     allowedHosts: [
       'https://portal-gateway-cmc.herokuapp.com/',
     ]
-  }
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
